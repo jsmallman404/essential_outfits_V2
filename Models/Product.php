acfb2247dbca06d80a01a37dccab60c044072226
jsmallman404 <jakesmallman04@gmail.com>
2025-02-04 18:51:23 +0000

    Add files via upload

---
+++ Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name','description','price','category','color','brand','image'];
+
+    public function variants ()
+    {
+        return $this -> hasMany(ProductVariant::class);
+    }
+}
