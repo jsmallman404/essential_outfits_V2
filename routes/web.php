84c75e3a736d321ab3f20bc6b400976bcbbe8bc6
jsmallman404 <jakesmallman04@gmail.com>
2025-02-04 19:06:46 +0000

    Revert "Products and Shopping Cart"

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\ProductController;
-
 
 Route::get('/', function () {
     return view('homepage');
@@ -11,22 +8,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-//Homepage Launcher (Defult Page)
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-//Shopping Cart Logic
-Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-Route::post('/cart/add/{id}', [CartController::class, 'addToCart'])->name('cart.add');
-Route::post('/cart/remove/{id}', [CartController::class, 'removeFromCart'])->name('cart.remove');
-Route::get('/cart/add/{id}', [CartController::class, 'addToCart'])->name('cart.add');
-Route::get('/cart/remove/{id}', [CartController::class, 'removeFromCart'])->name('cart.remove');
-
-
-//Product Page Logic
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-
-// Product Management
-Route::get('/admin/products', [ProductController::class, 'adminIndex'])->name('admin.products');
-Route::get('/admin/products/create', [ProductController::class, 'create'])->name('admin.products.create');
-Route::post('/admin/products/store', [ProductController::class, 'store'])->name('admin.products.store');
-
