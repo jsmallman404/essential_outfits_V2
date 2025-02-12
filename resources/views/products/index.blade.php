63a0b5aca7a2fbcb596959e663e2ea5e6aa88c7d
jsmallman404 <jakesmallman04@gmail.com>
2025-02-12 12:24:52 +0000

    Allowing images to be viewed

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -18,7 +18,7 @@
             @foreach($products as $product)
                 <div class="col-md-4 mb-4">
                     <div class="card">
-                        <img src="{{ asset('images/' . $product->image) }}" class="card-img-top">
+                        <img src="{{ asset('storage/' . $product->image) }}" class="card-img-top">
                         <div class="card-body">
                             <h5 class="card-title">{{ $product->name }}</h5>
                             <p class="card-text">£{{ $product->price }}</p>
