ea7d3b2f70fcc1021dbbee5fb3b4836076317f3e
leon77778 <160520047+leon77778@users.noreply.github.com>
2025-02-26 12:12:55 +0000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
-
     theme: {
         extend: {
             fontFamily: {
@@ -18,6 +16,5 @@ export default {
             },
         },
     },
-
-    plugins: [forms, typography],
+    plugins: [],
 };
