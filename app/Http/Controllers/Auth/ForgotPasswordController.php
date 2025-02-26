2924056f53b587f9a5e929fa6cf768cdc22db91f
girthangs <gsgirthan@gmail.com>
2025-02-26 11:48:03 +0000

    forgot password code

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -7,16 +7,22 @@ use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
 class ForgotPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset emails and
-    | includes a trait which assists in sending these notifications from
-    | your application to your users. Feel free to explore this trait.
-    |
-    */
+        public function showLinkRequestForm()
+        {
+            return view('auth.passwords.email');
+        }
+        public function sendResetLinkEmail(Request $request)
+        {
+            $request->validate(['email' => 'required|email|exists:users,email']);
+    
+            $status = Password::sendResetLink(
+                $request->only('email')
+            );
+    
+            return $status === Password::RESET_LINK_SENT
+                ? back()->with(['status' => __($status)])
+                : back()->withErrors(['email' => __($status)]);
+        }
 
     use SendsPasswordResetEmails;
 }
